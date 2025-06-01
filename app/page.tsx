0781876462ed7b1ef931f40bae95472109ba330e
adityaekasantoso <adityaekasantoso12@gmail.com>
2025-06-01 15:29:11 +0700

    add login-form

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,31 @@
-'use client';
+import { GalleryVerticalEnd } from "lucide-react"
+import { LoginForm } from "@/components/login-form"
 
-import PanelComponent from './components/PanelComponent';
-import MapComponents from './components/MapComponents';
-
-export default function Home() {
+export default function LoginPage() {
   return (
-    <div className="flex h-screen w-screen">
-      <div className="w-[22%] border-r border-gray-300">
-        <PanelComponent />
+    <div className="grid min-h-svh lg:grid-cols-2">
+      <div className="flex flex-col gap-4 p-6 md:p-10">
+        {/* <div className="flex justify-center gap-2 md:justify-start">
+          <a href="#" className="flex items-center gap-2 font-medium">
+            <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
+              <GalleryVerticalEnd className="size-4" />
+            </div>
+            Sistem Monitoring GPS
+          </a>
+        </div> */}
+        <div className="flex flex-1 items-center justify-center">
+          <div className="w-full max-w-xs">
+            <LoginForm />
+          </div>
+        </div>
       </div>
-
-      <div className="w-[78%]">
-        <MapComponents />
+      <div className="bg-muted relative hidden lg:block">
+        <img
+          src="/assets/bg.jpg"
+          alt="Image"
+          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+        />
       </div>
     </div>
-  );
+  )
 }
